fe81dad6d30e21cd7948e355b87c4f2d46f420f5
PyTanu09 <tanusonkarsimga9@gmail.com>
2025-04-26 09:55:58 +0530

    Delete Q14.R

--- Q14.R
+++
@@ -1,6 +0,0 @@
-##14. Extracting Only Female Entries From Factor Vector
-
-
-gender <- factor(c("male", "female", "female", "male", "female"))
-female_entries <- gender[gender == "female"]
-print(female_entries)
