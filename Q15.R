655c63d95fd838bfbf0e3f7bfb3a4265ac7be13d
PyTanu09 <tanusonkarsimga9@gmail.com>
2025-04-26 09:56:14 +0530

    Delete Q15.R

--- Q15.R
+++
@@ -1,6 +0,0 @@
-##15. Categorizing Numeric Data Using cut()
-
-
-num_vec <- c(5, 15, 25, 35, 45, 55)
-categories <- cut(num_vec, breaks = c(-Inf, 20, 40, Inf), labels = c("Low", "Medium", "High"))
-print(categories)
