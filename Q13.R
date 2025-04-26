562c58144d1e2e1900eda440dd7891036cc75cbc
PyTanu09 <tanusonkarsimga9@gmail.com>
2025-04-26 09:55:41 +0530

    Delete Q13.R

--- Q13.R
+++
@@ -1,5 +0,0 @@
-##13. Creating and Ordering Factor for Months
-
-
-months <- factor(c("Jan", "Mar", "Feb", "Apr", "May", "Dec", "Nov"), levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), ordered = TRUE)
-print(months)
