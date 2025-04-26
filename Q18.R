c2068aa5cc6947de19f542a5d524b0999685dcdd
PyTanu09 <tanusonkarsimga9@gmail.com>
2025-04-26 09:57:12 +0530

    Delete Q18.R

--- Q18.R
+++
@@ -1,8 +0,0 @@
-##18. Replacing Values Less Than Mean With NA
-
-
-num_vec <- c(5, 10, 15, 20, 25)
-mean_value <- mean(num_vec)
-new_vec <- ifelse(num_vec < mean_value, NA, num_vec)
-print(mean_value)
-print(new_vec)
