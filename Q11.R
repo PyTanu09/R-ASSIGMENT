c766b262997bea3cb217d9928a9df94988bb36c3
PyTanu09 <tanusonkarsimga9@gmail.com>
2025-04-26 09:55:11 +0530

    Delete Q11.R

--- Q11.R
+++
@@ -1,9 +0,0 @@
-## Using sub() and gsub()
-
-
-text <- "apple, apple, and apple"
-new_text_sub <- sub("apple", "orange", text)
-new_text_gsub <- gsub("apple", "orange", text)
-print(new_text_sub)
-print(new_text_gsub)
-
